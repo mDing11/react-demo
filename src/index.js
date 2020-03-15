a8ce23307fe70b1eafae3e2cd580103b0b7b6840
yufan <yufan@pinduoduo.com>
2020-03-15 11:29:37 +0800

    git rebase2

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route,Link, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import HomePage from './HomePage';
 // import { } from "antd";
 
-const Home =() => <div>首页</div>
+const Home = () => <div>首页</div>
 const UsersPage = () => <div>Users Page</div>
 const Car = () => <div>Car</div>
 
 const App = () => (
   <BrowserRouter>
-  <header>
-    <Link to="/">首页</Link>
-    <Link to="/users">用户</Link>
-    <Link to="/shop">商铺</Link>
-  </header>
+    <header>
+      <Link to="/">首页111</Link>
+      <Link to="/users">用户111</Link>
+      <Link to="/shop">商铺1111</Link>
+    </header>
     <div className="primary-layout">
       <Switch>
-      <Route exact path="/"  component={Home} />
-      <Route path="/shop"  component={HomePage} />
-      <Route path="/users" component={UsersPage} />
-      <Route path="/car" component={Car} />
+        <Route exact path="/" component={Home} />
+        <Route path="/shop" component={HomePage} />
+        <Route path="/users" component={UsersPage} />
+        {/* <Route path="/car" component={Car} /> */}
       </Switch>
-  </div>
+    </div>
   </BrowserRouter>
 )
 
