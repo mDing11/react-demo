9bbbfb256c5355cbcafee351dc4fd41865b24e09
yufan <yufan@pinduoduo.com>
2020-03-15 10:59:04 +0800

    try git rebase

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,27 +1,28 @@
 import React from "react";
-import {Route,Link,Switch,useRouteMatch} from 'react-router-dom';
+import { Route, Link, Switch, useRouteMatch } from 'react-router-dom';
 
 const HomePage = props => {
-    const List =() => <div>订单</div>
+    const List = () => <div>订单</div>
     const Shop = () => <div>商铺</div>
 
     // let { path, url } = useRouteMatch();
-return (<div>
-    {console.log('props',props)}
-    <div >
-        <Link to={`/shop/list`}>订单</Link>
-        <Link to='/car'>购物车</Link>
-    </div>
-    <div>
-        <Switch>
-            {console.log(props.match.path)}
-        {/* <Route exact path={path}>
-          <h3>Please select a topic.</h3>
-        </Route> */}
-        <Route path={`/shop/list`} component={List}/>
-        {/* <Route path={`/shop`} component={Shop}/> */}
-        </Switch>
-    </div>
-</div>)}
+    return (<div>
+        {console.log('props', props)}
+        <div >
+            <Link to={`/shop/list`}>订单</Link>
+            <Link to='/car'>购物车</Link>
+        </div>
+        <div>
+            <Switch>
+                {console.log(props.match.path)}
+                <Route exact path={path}>
+                    <h3>Please select a topic.</h3>
+                </Route>
+                <Route path={`/shop/list`} component={List} />
+                {/* <Route path={`/shop`} component={Shop}/> */}
+            </Switch>
+        </div>
+    </div>)
+}
 
 export default HomePage;
